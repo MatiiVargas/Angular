b0158567eb21dce6505fa49ff28a474c125bd50b
MatiiVargas <matias_vargas@live.com>
2022-05-21 00:57:52 -0300

    Empezando por el Header

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { BannerComponent } from './components/banner/banner.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LogoAPComponent,
+    RedesComponent,
+    BannerComponent,
+
   ],
   imports: [
     BrowserModule,
