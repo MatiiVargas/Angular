8c8b421461ccdaf1a84771fbcabb975db40f1488
MatiiVargas <matias_vargas@live.com>
2022-05-21 13:57:52 -0300

    Sección "hard & soft skills"

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -10,6 +10,9 @@ import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
+import { SkillsComponent } from './components/skills/skills.component';
+
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 
 
@@ -23,11 +26,13 @@ import { EducacionComponent } from './components/educacion/educacion.component';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
+    SkillsComponent,
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({}),
   ],
   providers: [],
   bootstrap: [AppComponent]
