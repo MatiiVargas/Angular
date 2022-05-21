bd730759a3638bc29961e6385c22a7b4ac2afd60
MatiiVargas <matias_vargas@live.com>
2022-05-21 20:21:11 -0300

    Creación de carpeta Backend

---
+++ Portfolio/Backend/src/main/java/com/portfolio/matias/MatiasApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.matias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatiasApplication.class, args);
+	}
+
+}
